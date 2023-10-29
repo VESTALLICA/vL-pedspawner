8b7295ca755d72631b8aadefff4c55624a2b86f1
VESTA <147888622+VESTALLICA@users.noreply.github.com>
2023-10-29 02:59:31 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -14,6 +14,7 @@ Config.Peds = {
         ["hostile"] = false, -- will auto attack player
         ["ragdoll"] = false, -- if true then ped will not ragdoll at all. does not work if entity position is frozen
         ["freeze"] = false,  -- freeze entity position
+        ["blind"] = false, -- ped will not react to anything and will stay still when hit. 
 
         -- COMBAT & PATHING --
         ["range"] = 1, -- combat range. 0 = near, 1 = medium, 2 = far
@@ -30,7 +31,14 @@ Config.Peds = {
         ["climb"] = true, -- if ped can climb and vault things
         ["drop"] = true, -- if ped can drop from heights
         ["route"] = false, -- if the ped has a patrol route it should follow. define routes in routes.lua.
+        
+        ["wander"] = false, -- route must be false. ped will instead wander around 
+        ["radius"] = 0.0, -- wander in spawned area with radius of 100 meters. must end in decimal value.
+        ["walk"] = 5, -- distance in meters that the ped will walk
+        ["wait"] = 10.0, -- how long the ped will wait after walking X meters ^
 
+        ["scenario"] = { 'WORLD_HUMAN_HIKER' }, -- some scenarios that the ped can play on spawn. wander and route must be off. ped can be brought out of anim if not blind. list of gta scenarios: pastebin.com/6mrYTdQv
+        --["scenario"] = false, -- leave false if no scenario. 
 
         -- WEAPONS --
         ["primary"] = 'assaultrifle', -- input just weapon: 'assaultrifle' 'microsmg' 'appistol'. weapon_ is predefined in the code
